602243b5e6c1b851d8403f8f81273e8a184f0de4
KimMinchan95 <hialscks@gmail.com>
2022-10-08 00:02:37 +0900

    문자열

---
+++ chapter4/탈출문자.py
@@ -0,0 +1,25 @@
+# 문자열의 줄을 바꿈하고 싶어서 엔터를 치면 에러가 발생
+
+# 잘못된 예시
+# print("백문이 불여일견 
+# 백견이 불여일타") Error
+
+# 옳은 예시
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+# 저는 "나도코딩" 입니다.
+print("저는 \"나도코딩\"입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\himin\\coding\\studyPython>")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
